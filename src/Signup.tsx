8780efe081d10cc457196a03c5d0060bf825643a
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-09-11 09:38:56 +0200

    Update Signup.tsx
    I change names of divs and added a button
    I added import for style so I can be able to style the page

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -1,27 +1,34 @@
+import React from 'react';
+import pic1 from './assets/side-pic.png';
+import './Styles.css'; 
 
-import pic1 from './assets/side-pic.png'
+const Signup: React.FC = () => {
+  return (
+    <div className="signup-div">
+      <div className="form-div">
+        <h1>Sign up with email</h1>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input type="email" className="input" />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input type="password" className="input" />
+          </label>
+          <label htmlFor="confirm-password">
+            Confirm Password:
+            <input type="password" className="input" />
+          </label>
+          <button type="submit" className="submit-button">Sign Up</button>
+        </form>
+      </div>
 
+      <div className="image-div">
+        <img src={pic1} alt="Side Picture" />
+      </div>
+    </div>
+  );
+};
 
-export default function Signup(){
-    
-    return(
-        <>
-        <div className="everything">
-            <div className="form-div">
-                <h1>Sign in with email</h1>
-
-            <form action="">
-             <label htmlFor="email">Email: <input type="email" className="email"/></label>
-            <label htmlFor="password">Password: <input type="password" className="password"/></label>
-            <label htmlFor="password">Confirm Password: <input type="password" className="password1"/></label>
-             <button>Sign Up</button>
-            </form>
-           
-            </div>
-            <div className="image-div">
-                <img src={pic1} alt="" />
-            </div>
-        </div>
-        </>
-    )
-}
+export default Signup;
