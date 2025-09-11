eeed5cf88d6fbb4cf159509bfd79daab3eb1ec6d
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-09-11 12:35:39 +0200

    Update SignIn.tsx
    I added required for input fields

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,27 +1,44 @@
-import { Link } from "react-router-dom";
-import pic1 from './assets/side-pic.png'
+import React from 'react';
+import pic1 from './assets/side-pic.png';
+import './Styles.css';
 
+const SignIn: React.FC = () => {
+  return (
+    <div className="all">
+      <h1 className="signup-heading">Sign in with email</h1>
+      <div className="signup-div">
 
+        <div className="form-div">
+          <form>
+            <label htmlFor="email">
+              Email:
+              <input type="email" className="input" readOnly/>
+            </label>
 
-export default function signIn(){
-    return(
-        <>
-        <div className="everything">
-            <div className="form-div">
-                <h1>Sign in with email</h1>
+            <label htmlFor="password">
+              Password:
+              <input type="password" className="input" required/>
+            </label>
 
-            <form action="">
-             <label htmlFor="email">Email: <input type="email" className="email"/></label>
-            <label htmlFor="password">Password: <input type="password" className="password"/></label>
-            <label htmlFor="privacy"><input type="checkbox" className="terms"/>I agree to the <Link to="">Terms & Privacy</Link></label>
-            <button>Sign In</button>
-            </form>
-            <label htmlFor="signup">Don't have account? <button>Sign Up</button></label>
-            </div>
-            <div className="image-div">
-                <img src={pic1} alt="" />
-            </div>
+            <label htmlFor="privacy" className="checkbox-label">
+              <input type="checkbox" className="checkbox" required/>
+              I agree to the Terms & Privacy
+            </label>
+
+            <button type="submit" className="submit-btn">Sign In</button>
+
+            <p className="redirect-text">
+              Don't have an account? <button className="link-button">Sign Up</button>
+            </p>
+          </form>
         </div>
-        </>
-    );
-}
+
+        <div className="image-div">
+          <img src={pic1} alt="Side visual" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
