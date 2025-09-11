e5541320b23ddd9ced6af5c1fce197cdc3879176
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-09-11 09:14:06 +0200

    Update App.tsx
    I have removed other pages because it was showing a blank pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
-import Signup from './signup'; 
+
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/Signup" element={<Signup />} />
-      </Routes>
-    </Router>
+   <LandingPage />
   );
 };
 
