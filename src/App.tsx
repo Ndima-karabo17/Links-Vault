dc8a5c313ba917839dbc7ca813812a640c82f730
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2026-01-15 09:26:05 +0200

    Update App.tsx
    I added routers to be able to navigate to other pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
+import Signin from './SignIn';          
+import Signup from './Signup';
+import AddLink from './AddLink';
+import LinkView from './LinkView';
 
-
-const App: React.FC = () => {
+function App() {
   return (
-   <LandingPage />
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<LandingPage />} />
+        
+     
+        <Route path="/signin" element={<Signin />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='add-link' element={< AddLink/>} />
+        <Route path='link-view' element={<LinkView/>} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
